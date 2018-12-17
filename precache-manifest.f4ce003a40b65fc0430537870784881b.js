9d693b59f10d256efa4df5bcf8c2d765449c72a6
jose rodolfo perez <thejoserodolfo@gmail.com>
2018-12-17 04:04:34 -0800

    Updates

---
+++ precache-manifest.f4ce003a40b65fc0430537870784881b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e0f7e98d585c5b83365",
+    "url": "/joserodolfo/static/css/main.76e577f1.chunk.css"
+  },
+  {
+    "revision": "1e0f7e98d585c5b83365",
+    "url": "/joserodolfo/static/js/main.1e0f7e98.chunk.js"
+  },
+  {
+    "revision": "66189330bfa3d44643d8",
+    "url": "/joserodolfo/static/js/1.66189330.chunk.js"
+  },
+  {
+    "revision": "f980b8be2a5a8dc69712",
+    "url": "/joserodolfo/static/js/runtime~main.f980b8be.js"
+  },
+  {
+    "revision": "e04dd8138d8447f606e3cf214da5e2d1",
+    "url": "/joserodolfo/static/media/le-buzz-568591-unsplash.e04dd813.jpg"
+  },
+  {
+    "revision": "99db30ed888f32c4a5f44ca54b2da9c2",
+    "url": "/joserodolfo/static/media/Logo-Black (3).99db30ed.png"
+  },
+  {
+    "revision": "8830b4aaa9e77e467da7692b4034d283",
+    "url": "/joserodolfo/static/media/me_head.8830b4aa.jpg"
+  },
+  {
+    "revision": "c8ec98f638350bef08093b10d90a8901",
+    "url": "/joserodolfo/index.html"
+  }
+];
